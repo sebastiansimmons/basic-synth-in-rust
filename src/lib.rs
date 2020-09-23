b28cb10bb2343660de1dfe3bfcf0c29e522b98b1
Sebastian <sebastianctsimmons@gmail.com>
2020-09-23 01:10:37 -0700

    vst now generates white noise

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate vst;
 
 use vst::plugin::{Info, Plugin, Category};
+use vst::buffer::AudioBuffer;
+use rand::random;
 
 #[derive(Default)]
 struct Whisper;
@@ -25,6 +27,20 @@ impl Plugin for Whisper {
             // Fill the rest with default values
             ..Default::default()
         }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        let (_, mut output_buffer) = buffer.split();
+
+        for output_channel in output_buffer.into_iter() {
+            // Let's iterate over every sample in our channel.
+            for output_sample in output_channel {
+                // For every sample, we want to add a random value from
+                // -1.0 to 1.0.
+                *output_sample = (random::<f32>() - 0.5f32) * 2f32;
+            }
+        }
+
     }
 }
 
